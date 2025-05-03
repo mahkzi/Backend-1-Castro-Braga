9b4edc492007633aa5a851f4c6426c61be54562e
Maximo Castro <maximocastrobraga@gmail.com>
2025-05-03 10:48:59 -0300

    comenzando con las vistas, pero no esta la lógica de la vista dinámica

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const viewsRouter = require("./routes/vistas.router");
 app.use("/", viewsRouter);
 
-const socketManager = require("./utils/socketManager");
+const socketManager = require("./utils/socketManager.js");
 socketManager(io);
