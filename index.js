ece2164e384a71d84bdf5d111d52cd200c7b0bb8
Maximo Castro <maximocastrobraga@gmail.com>
2025-05-03 16:16:14 -0300

    pruebas de btn

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
+const express = require("express");
+const {Server} = require ("socket.io");
+const handlebars = require ("express-handlebars");
+const path = require ("path");
 const viewsRouter = require("./routes/vistas.router");
-app.use("/", viewsRouter);
-
 const socketManager = require("./utils/socketManager.js");
-socketManager(io);
+const app = express();
+const server = require ("http").createServer(app);
+const io = new Server(server);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.engine("hanldebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+
